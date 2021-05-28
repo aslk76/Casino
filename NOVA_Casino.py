da541fa6640d43a9b6e80602d030e45161415a9c
aslk76 <q_shishani@yahoo.com>
2021-05-28 11:00:45 +0300

    lottery embed message ID set

--- NOVA_Casino.py
+++ NOVA_Casino.py
@@ -668,7 +668,7 @@ async def lottery(ctx):
                     await cursor.execute(query)
                     (lottery_pot,) = await cursor.fetchone()
                     async for message in lottery_channel.history(limit=50, oldest_first=True):
-                        if message.id == 716560494160248893:
+                        if message.id == 847745930563944468:
                             lottery_msg = message
                             lottery_embed_pre = message.embeds[0].to_dict()
                             lottery_embed_pre_fields = lottery_embed_pre['fields']
@@ -698,7 +698,7 @@ async def resetEmbed(ctx, price: str):
     await ctx.message.delete()
     lottery_channel = get(ctx.guild.text_channels, id=815104636708323331)
     async for message in lottery_channel.history(limit=50, oldest_first=True):
-        if message.id == 716560494160248893:
+        if message.id == 847745930563944468:
             lottery_msg = message
             lottery_embed_pre = message.embeds[0].to_dict()
             lottery_embed_pre_fields = lottery_embed_pre['fields']
